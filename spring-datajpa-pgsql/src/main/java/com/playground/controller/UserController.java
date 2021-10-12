61ca81c5203a0d357ce62bbdbc882123b1c23feb
gokhangurbuz <gokhan.gurbuz@getir.com>
2021-10-12 18:24:22 +0300

    spring-datajpa-pgsql swagger added.

--- spring-datajpa-pgsql/src/main/java/com/playground/controller/UserController.java
+++ spring-datajpa-pgsql/src/main/java/com/playground/controller/UserController.java
@@ -2,6 +2,9 @@ package com.playground.controller;
 
 import com.playground.dto.UserDto;
 import com.playground.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +14,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Api(value = "User Api documentations")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserDto> kaydet(@RequestBody UserDto userDto){
+    @ApiOperation(value = "Add new user method", notes = "The method will be remove next release!")
+    public ResponseEntity<UserDto> kaydet(@RequestBody @ApiParam(value = "user") UserDto userDto){
         return ResponseEntity.ok(userService.save(userDto));
     }
 
     @GetMapping
+    @ApiOperation(value = "Get all users method")
     public ResponseEntity<List<UserDto>> tumunuListele() {
         return ResponseEntity.ok(userService.getAll());
     }
--- spring-datajpa-pgsql/src/main/java/com/playground/dto/UserDto.java
+++ spring-datajpa-pgsql/src/main/java/com/playground/dto/UserDto.java
@@ -1,17 +1,24 @@
 package com.playground.dto;
 
 import com.playground.entity.Address;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Api(value = "User object")
 public class UserDto {
+    @ApiModelProperty(value = "id field")
     private Long id;
 
+    @ApiModelProperty(value = "name field")
     private String name;
 
+    @ApiModelProperty(value = "surname field")
     private String surname;
 
+    @ApiModelProperty(value = "addressList field")
     private List<String> addressList;
 }
